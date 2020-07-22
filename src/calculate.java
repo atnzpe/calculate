40069eb5a059bde5d28edf6335af7de4ce2de9ce
Gleyson Atanazio <gleysonatanazio@gmail.com>
2020-07-21 21:43:59 -0300

    funçoes SUM e MINUS reescritas

--- src/calculate.java
+++ src/calculate.java
@@ -3,19 +3,18 @@ public class calculate {
         int x = Integer.parseInt(args[1]);
         int y = Integer.parseInt(args[2]);
 
-        if(args[0].equals("somar")){
-            sum (x,y);
-        } else if (args[0].equals("subtrair")){
-            minus (x,y);
+        if (args[0].equals("somar")) {
+            sum(x, y);
+        } else if (args[0].equals("subtrair")) {
+            minus(x, y);
         }
-
-
     }
 
-        static void sum(int x, int y){
-            System.out.println(x + y);
-        }
-        static void minus(int x, int y){
-            System.out.println(x -x);
-        }
+    static void sum(int x, int y) {
+        System.out.println(x + y);
+    }
+
+    static void minus(int x, int y) {
+        System.out.println(x - y);
+    }
 }
